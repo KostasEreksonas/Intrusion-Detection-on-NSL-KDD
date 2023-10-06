d99ba60eeae2a3115ffb78437bcb7cc37656594d
KostasEreksonas <k.ereksonas@gmail.com>
2023-10-06 19:28:40 +0300

    Translated the program

--- run.py
+++ run.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
-#  --------
-# |Šaltinis|
-#  --------
-
-# Source: https://github.com/hymoe/Intrusion-Detection-on-NSL-KDD
-
-#  -------------------------
-# |Importuojamos bibliotekos|
-#  -------------------------
-
 import os
 import csv
 import time
@@ -23,17 +13,17 @@ from tensorflow.keras.utils import to_categorical, plot_model
 from sklearn.preprocessing import Normalizer
 import matplotlib.pyplot as plt
 
-#  -----------------------
-# |Veikimo laiko matavimas|
-#  -----------------------
+#  ----------------------------
+# | Execution time measurement |
+#  ----------------------------
 start = time.perf_counter()
 
-#  -------------------
-# |Duomenų apdorojimas|
-#  -------------------
+#  --------------------
+# | Data preprocessing |
+#  --------------------
 
-# Visi 39 atakų tipai suklasifikuojami į 4 klases
-print("[+] NSL-KDD tinklo duomenų rinkinyje esančių atakų tipų klasifikacija į 4 klases")
+# Attack type classification
+print("[+] NSL-KDD data classification to 4 attack data types")
 print("--------------------------------------------------------------------------------")
 dos_type = ['back','land','neptune','pod','smurf','teardrop','processtable','udpstorm','mailbomb','apache2']
 probing_type = ['ipsweep','mscan','nmap','portsweep','saint','satan']
@@ -49,36 +39,36 @@ for i in u2r_type:
     type2id[i] = 3
 for i in probing_type:
     type2id[i] = 4
-print(f"Klasifikuoti atakų tipai: {type2id}\n")
+print(f"Classified attack data types: {type2id}\n")
 
-# Kiekvienam protokolo tipui priskiriamas eilės numeris
-print("[+] NSL-KDD tinklo duomenų rinkinyje esantys protokolų tipai")
+# Every protocol type within NSL-KDD gets it's id
+print("[+] NSL-KDD dataset protocol types")
 print("------------------------------------------------------------")
 all_protocol = ['tcp', 'udp', 'icmp']
 protocol_dict = {}
 for id,name in enumerate(all_protocol):
     protocol_dict[name] = id
-print(f"Protokolų tipai: {protocol_dict}\n")
+print(f"Protocol types: {protocol_dict}\n")
 
-# Kiekvienam paslaugos tipui priskiriamas eilės numeris
-print("[+] NSL-KDD tinklo duomenų rinkinyje esantys paslaugų tipai")
+# Every service type within NSL-KDD gets it's id
+print("[+] NSL-KDD dataset service types")
 print("-----------------------------------------------------------")
 all_service = ['aol', 'auth', 'bgp', 'courier', 'csnet_ns', 'ctf', 'daytime', 'discard', 'domain', 'domain_u', 'echo', 'eco_i', 'ecr_i', 'efs', 'exec', 'finger', 'ftp', 'ftp_data', 'gopher', 'harvest', 'hostnames', 'http', 'http_2784', 'http_443', 'http_8001', 'imap4', 'IRC', 'iso_tsap', 'klogin', 'kshell', 'ldap', 'link', 'login', 'mtp', 'name', 'netbios_dgm', 'netbios_ns', 'netbios_ssn', 'netstat', 'nnsp', 'nntp', 'ntp_u', 'other', 'pm_dump', 'pop_2', 'pop_3', 'printer', 'private', 'red_i', 'remote_job', 'rje', 'shell', 'smtp', 'sql_net', 'ssh', 'sunrpc', 'supdup', 'systat', 'telnet', 'tftp_u', 'tim_i', 'time', 'urh_i', 'urp_i', 'uucp', 'uucp_path', 'vmnet', 'whois', 'X11', 'Z39_50']
 service_dict = {}
 for id,name in enumerate(all_service):
     service_dict[name] = id
-print(f"Paslaugų tipai: {service_dict}\n")
+print(f"Service types: {service_dict}\n")
 
-# Kiekvienai vėliavos (flag) vertei priskiriamas eliės numeris
-print("[+] NSL-KDD tinklo duomenų rinkinyje esantčios vėliavos vertės")
+# Every flag within NSL-KDD gets it's id
+print("[+] NSL-KDD dataset flag types")
 print("--------------------------------------------------------------")
 all_flag = ['OTH', 'REJ', 'RSTO', 'RSTOS0', 'RSTR', 'S0', 'S1', 'S2', 'S3', 'SF', 'SH']
 flag_dict = {}
 for id,name in enumerate(all_flag):
     flag_dict[name] = id
-print(f"Vėliavos: {flag_dict}\n")
+print(f"Flags: {flag_dict}\n")
 
-# Apmokymo duomenų nuskaitymas
+# Read training data
 all_train_data = []
 trainX = []
 trainY = []
@@ -86,11 +76,11 @@ with open(os.getcwd()+'/NSL-KDD/KDDTrain+.txt', newline='') as trainingData:
     trainingData = csv.reader(trainingData, delimiter=',')
     for row in trainingData:
         all_train_data.append(row) # Duomenys nuskaitomi eilutė po eilutės
-print("[+] Apmokymo duomenys, paimti iš duomenų rinkinio")
+print("[+] Training data from NSL-KDD")
 print("-------------------------------------------------")
 print(all_train_data[0],"\n")
 
-# Tekstinės duomenų reikšmės pakeičiamos jų koduotomis vertėmis
+# Encoding training data
 for i in all_train_data:
     i[1] = protocol_dict[i[1]]
     i[2] = service_dict[i[2]]
@@ -98,14 +88,14 @@ for i in all_train_data:
     i[-2] = type2id[i[-2]]
     trainX.append(i[:41])
     trainY.append(i[-2])
-print("[+] Užkoduoti apmokymo duomenys")
+print("[+] Encoded training data")
 print("-------------------------------")
 print(f"{trainX[0]}\n")
-print("[+] Užkoduotas kibernetinės atakos tipas (0-5)")
+print("[+] Encoded cyber attack type (0-5)")
 print("----------------------------------------------")
 print(trainY[0],"\n")
 
-# Testavimo duomenų nuskaitymas
+# Reading testing data
 all_test_data = []
 testX = []
 testY = []
@@ -113,11 +103,11 @@ with open(os.getcwd()+'/NSL-KDD/KDDTest+.txt', newline='') as testData:
     testData = csv.reader(testData, delimiter=',')
     for row in testData:
         all_test_data.append(row) # Duomenys nuskaitomi eilutė po eilutės
-print("[+] Testavimo duomenys, paimti iš duomenų rinkinio")
+print("[+] Testing data from NSL-KDD")
 print("--------------------------------------------------")
 print(all_test_data[0],"\n")
 
-# Tekstinės duomenų reikšmės pakeičiamos jų koduotomis vertėmis
+# Encoding testing data
 for i in all_test_data:
     i[1] = protocol_dict[i[1]]
     i[2] = service_dict[i[2]]
@@ -126,93 +116,93 @@ for i in all_test_data:
     testX.append(i[:41])
     testY.append(i[-2])
 
-print("[+] Užkoduoti testavimo duomenys")
+print("[+] Encoded testing data")
 print("--------------------------------")
 print(testX[0],"\n")
-print("[+] Užkoduotas kibernetinės atakos tipas (0-5)")
+print("[+] Encoded cyber attack type (0-5)")
 print("----------------------------------------------")
 print(testY[0],"\n")
 
-# Duomenų parengimas (preprocessing)
+# Data preprocessing
 trainX = Normalizer().fit_transform(trainX)
-print("\n[+] Transformuoti apmokymo duomenys")
+print("\n[+] Transformed training data")
 print("-----------------------------------")
 print(trainX,"\n")
 testX = Normalizer().fit_transform(testX)
-print("[+] Transformuoti testavimo duomenys")
+print("[+] Transformed testing data")
 print("------------------------------------")
 print(testX,"\n")
 
-# Duomenų transformavimas į 3D masyvą
+# Data transformation to 3D array
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-print("[+] Apmokymo duomenys transformuoti į 3D masyvą")
+print("[+] Training data transformed to 3D array")
 print("-----------------------------------------------")
 print(trainX,"\n")
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-print("[+] Testavimo duomenys transformuoti į 3D masyvą")
+print("[+] Testing data transformed to 3D array")
 print("------------------------------------------------")
 print(testX,"\n")
 
-# Kompiuterinio tinklo klasių kodavimas one-hot metodu
+# One-hot encoding of classified data
 trainY = tf.keras.utils.to_categorical(trainY, num_classes=5)
-print("[+] Apmokymo duomenų rinkinyje esančių atakų klasių kodavimas one-hot metodu")
+print("[+] Encoding NSL-KDD training data of attack classes by one-hot method")
 print("----------------------------------------------------------------------------")
 print(trainY,"\n")
 testY = tf.keras.utils.to_categorical(testY, num_classes=5)
-print("[+] Testavimo duomenų rinkinyje esančių atakų klasių kodavimas one-hot metodu")
+print("[+] Encoding NSL-KDD testing data of attack classes by one-hot method")
 print("-----------------------------------------------------------------------------")
 print(testY,"\n")
 
-print("[+] Duomenys parengti\n")
+print("[+] Data is prepared\n")
 
-#  -----------------
-# |Duomenys parengti|
-#  -----------------
+#  ------------------
+# | Data is prepared |
+#  ------------------
 
 #  -------
-# |Modelis|
+# | Model |
 #  -------
 
-# Modelio sukūrimas
+# Creating model
 model = Sequential()
-model.add(GRU(256, input_shape=(1,41)))     # Gated Recurrent Unit, skirtas saugoti aktualią tinklui informaciją
+model.add(GRU(256, input_shape=(1,41)))     # Gated Recurrent Unit
 model.add(Dropout(0.2))
-model.add(Dense(64, activation='relu'))     # Paslėptas sluoksnis #1
+model.add(Dense(64, activation='relu'))     # Hidden layer #1
 model.add(Dropout(0.2))
-model.add(Dense(64, activation='relu'))     # Paslėptas sluoksnis #2
+model.add(Dense(64, activation='relu'))     # Hidden layer #2
 model.add(Dropout(0.2))
-model.add(Dense(5, activation='softmax'))   # Išeigos sluoksnis
+model.add(Dense(5, activation='softmax'))   # Output layer
 
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
 
-print("\n[+] Modelio apmokymas bei validavimas")
+print("\n[+] Model training and validation")
 print("---------------------------------------")
 history = model.fit(trainX, trainY,
           epochs=20,
           batch_size=16, validation_split=0.1, verbose=1)
 
-print("\n[+] Modelio testavimas")
+print("\n[+] Model testing")
 print("------------------------")
 _, score = model.evaluate(testX, testY, batch_size=16)
-print("\n[+] Modelio tikslumas")
+print("\n[+] Modelio accuracy")
 print("-----------------------")
-print('Tikslumas: %.2f' % (score*100)+"%")
+print('Accuracy: %.2f' % (score*100)+"%")
 
 #  --------
-# |Grafikai|
+# | Graphs |
 #  --------
 
-print("\n[+] Sukurto neuroninio tinklo modelio grafikai")
+print("\n[+] Graphs of a created neural network")
 print("------------------------------------------------")
-print("[+] Kuriama neuroninio tinklo modelio blokinė schema")
+print("[+] Neural network block scheme")
 plot_model(model, to_file='Models/model.png')
-print("[+] Schema išsaugota adresu:"+os.getcwd()+"/Models/model.png")
+print("[+] Graph saved at: "+os.getcwd()+"/Models/model.png")
 
-# Grafike pavaizduojamos apmokymo bei validavimo tikslumo reikšmės
-print("[+] Kuriamas neuroninio tinklo modelio tikslumo grafikas")
+# Graph of training and validation accuracy values
+print("[+] Neural network accuracy graph")
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('Model accuracy')
@@ -220,10 +210,10 @@ plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.savefig(os.getcwd()+'/Models/acc.png')
-print("[+] Grafikas išsaugotas adresu:"+os.getcwd()+"/Models/acc.png")
+print("[+] Graph saved at: "+os.getcwd()+"/Models/acc.png")
 
-# Apmokymo bei validavimo praradimo (loss) reikšmės
-print("[+] Kuriamas praradimo (loss) vertės grafikas")
+# Graph of training and validation loss values
+print("[+] Neural network loss value graph")
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('Model loss')
@@ -231,18 +221,18 @@ plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.savefig(os.getcwd()+'/Models/loss.png')
-print("[+] Grafikas išsaugotas adresu:"+os.getcwd()+"/Models/loss.png")
+print("[+] Graph saved at: "+os.getcwd()+"/Models/loss.png")
 
-#  -----------------------
-# |Veikimo laiko matavimas|
-#  -----------------------
-print("\n[+] Programos veikimo laikas")
+#  ----------------------------
+# | Execution time measurement |
+#  ----------------------------
+print("\n[+] Execution time")
 print("------------------------------")
 end = time.perf_counter()
-laikas = end-start
-if (laikas < 60):
-    print("Programos veikimo laikas: %.2f" % laikas+" s")
-if (laikas > 60):
-    minutes = laikas / 60
-    sekundes = laikas - int(minutes) * 60
-    print("Programos veikimo laikas: %.0f" % minutes+" min. ir %.2f" % sekundes+" s")
+time = end-start
+if (time < 60):
+    print("Execution time: %.2f" % time+" s")
+if (time > 60):
+    minutes = time / 60
+    seconds = time - int(minutes) * 60
+    print("Execution time: %.0f" % minutes+" min. ir %.2f" % seconds+" s")
